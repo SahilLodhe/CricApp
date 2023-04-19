386d6f4b67280b26e8ae2615047d22aaf41c86b3
SahilLodhe <sahillodhe17@gmail.com>
2023-04-19 23:24:10 +0530

    modified the venv activating commmand yet again

--- envsetup.sh
+++ envsetup.sh
@@ -6,8 +6,8 @@ then
 else
     python3 -m venv env
 fi
-source .env/bin/activate
-# source env/bin/activate
+# source .env/bin/activate
+source env/bin/bash/activate
 
 
 pip3 install -r requirements1.txt
